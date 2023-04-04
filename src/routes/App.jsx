032332e3d814de7b8805339665fea7ccfaf8f71f
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-04-03 20:17:52 -0600

    repair error routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 	return (
 		<AppProvider>
 			<Layout>
-				<BrowserRouter>
+				<BrowserRouter basename='/react-shop'>
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/login' element={<Login />} />
