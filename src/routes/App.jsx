f350a81775e046a2e8dc048477b330d8bd9b7ac2
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-04-19 11:24:32 -0600

    [Error] add name domain

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 	return (
 		<AppProvider>
 			<Layout>
-				<BrowserRouter basename='/'>
+				<BrowserRouter basename='/reactshop.world'>
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/login' element={<Login />} />
