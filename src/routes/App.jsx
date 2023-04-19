87a0faa0a7e609b0043a125716c2032c5f044fb6
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-04-19 11:20:35 -0600

    [error] browser domain

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 	return (
 		<AppProvider>
 			<Layout>
-				<BrowserRouter>
+				<BrowserRouter basename='/'>
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/login' element={<Login />} />
