0bbc8e9e7e0ecab9d99c2b7e3440e1b7670787e8
Enrique-mdos <139999854+Enrique-mdos@users.noreply.github.com>
2024-10-31 20:37:25 -0600

    Fix: Ajuste en el icono  del producto.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: './',
+		publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
